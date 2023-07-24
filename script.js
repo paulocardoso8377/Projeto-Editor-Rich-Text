3c32bd743755a1a926b4384819da683e048b895c
Pedro Cardoso <135392261+paulocardoso8377@users.noreply.github.com>
2023-07-23 22:20:37 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,3 @@
+const $ = document.querySelector.bind(document)/* Selecionando esse documento quando usado
+essa const*/
+const divText = $('.text')/* Mencionando a const criada acima e selecionando a classe ".text" */
