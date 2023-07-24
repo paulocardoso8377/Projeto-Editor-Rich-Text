af04abb79926e149361b3375acb4c80c34c1c7b3
Pedro Cardoso <135392261+paulocardoso8377@users.noreply.github.com>
2023-07-23 23:15:04 -0300

    atualização js do projeto efrt

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const $ = document.querySelector.bind(document)/* Selecionando esse documento quando usado
 essa const*/
 const divText = $('.text')/* Mencionando a const criada acima e selecionando a classe ".text" */
+
+let lastInput 
+
+$('.t').addEventlistener('click', () => {
+
+    if(!lastInput) return alert('Nenhum componente selecionado')
+    if(lastInput.style.fontSize === '25px') lastInput.style.fontSize = '15px'
+    else lastInput.style.fontSize = '25px'
+
+})
