bb16e3aafdb4946c1eff32fd777845bf25d008ba
Pedro Cardoso <135392261+paulocardoso8377@users.noreply.github.com>
2023-07-24 21:55:49 -0300

    atualização EFRT

--- script.js
+++ script.js
@@ -11,3 +11,40 @@ $('.t').addEventlistener('click', () => {
     else lastInput.style.fontSize = '25px'
 
 })
+$('.b').addEventListener('click', () => {
+    if (!lastInput) return alert('Nenhum componente selecionado')
+    if (lastInput.style.fontWeight === 'bold') lastInput.style.fontWeight = ''
+    else lastInput.style.fontWeight = 'bold'
+})
+$('.i').addEventListener('click', () => {
+    if (!lastInput) return alert('Nenhum componente selecionado')
+    if (lastInput.style.fontStyle === "italic") lastInput.style.fontStyle = ""
+    else lastInput.style.fontStyle = "italic"
+})
+$('.u').addEventListener('click', () => {
+    if (!lastInput) return alert('Nenhum componente selecionado')
+    if (lastInput.style.textDecoration === "underline") {
+        lastInput.style.textDecoration = ''
+    }else  lastInput.style.textDecoration = 'underline'
+})
+$('.s').addEventListener('click', () => {
+    if (!lastInput) return alert('Nenhum componente selecionado')
+    if (lastInput.style.textDecoration === "line-through") {
+        lastInput.style.textDecoration = ''
+    }else  lastInput.style.textDecoration = 'line-through'
+})
+$('.link').addEventListener('click',() => {
+    if(!lastInput) return('Nenhum componente selecionado') 
+    const domain = ('Digite o link do domínio  que deseja selecionar\n obs: incluindo o https://')
+    if(!domain) return;
+    const content = lastInput.value
+    const container = lastInput.parentElement
+    lastInput.remove()
+    const link = document.createElement('a')
+    link.setAttribute('href', domain)
+    link.setAttribute('class', 'input-link')
+    link.innerHTML = content 
+    container.insertAdjacentElement('afterbegin', link)
+})
+
+    
